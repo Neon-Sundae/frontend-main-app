554ef719eec9dee83349466fd698825d81691db5
anubhavadarsh <anubhav.adarsh9@gmail.com>
2022-06-16 10:54:42 +0530

    feat: add tasks and projects section in home

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,20 @@
 import { FC, ReactElement } from 'react';
+import clsx from 'clsx';
 import styles from './index.module.scss';
 
 interface ComponentProps {
   children: ReactElement;
+  className?: string;
 }
 
-const Card: FC<ComponentProps> = ({ children }) => {
-  return <div className={styles.glass}>{children}</div>;
+const Card: FC<ComponentProps> = ({ children, className }) => {
+  return (
+    <div className={clsx(styles.glass, styles.card, className)}>{children}</div>
+  );
+};
+
+Card.defaultProps = {
+  className: '',
 };
 
 export default Card;
--- src/components/Home/Landing/index.tsx
+++ src/components/Home/Landing/index.tsx
@@ -1,13 +1,19 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import NavBar from 'components/NavBar';
+import Tasks from '../Tasks';
 import styles from './index.module.scss';
 import Banner from '../Banner';
+import Projects from '../Projects';
 
 const Landing: FC = () => {
   return (
     <div className={styles.background}>
       <NavBar />
       <Banner />
+      <section className={styles.content}>
+        <Projects />
+        <Tasks />
+      </section>
     </div>
   );
 };
