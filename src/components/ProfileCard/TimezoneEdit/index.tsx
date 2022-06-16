a91bd4f9b02ee0b4db0f9143e666ba39585075b1
GermaVinsmoke <vaibhav1180@gmail.com>
2022-06-16 13:19:34 +0530

    profile timezone update
    Signed-off-by: GermaVinsmoke <vaibhav1180@gmail.com>

--- src/components/ProfileCard/TimezoneEdit/index.tsx
+++ src/components/ProfileCard/TimezoneEdit/index.tsx
@@ -1,17 +1,24 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ReactComponent as LocationIcon } from 'assets/illustrations/profile/location.svg';
 import { useSelector } from 'react-redux';
 import { RootState } from 'reducers';
 import styles from './index.module.scss';
+import TimezoneModal from './TimezoneModal';
 
 const TimezoneEdit: FC = () => {
+  const [open, setOpen] = useState(false);
   const profile = useSelector((state: RootState) => state.profile.profile);
 
+  const handleOpenModal = () => setOpen(true);
+
   return (
-    <div className={styles['timezone-container']}>
-      <LocationIcon width={14} height={19.7} />
-      <span>{profile?.timezone ?? 'Singapore GMT+8'}</span>
-    </div>
+    <>
+      <div className={styles['timezone-container']} onClick={handleOpenModal}>
+        <LocationIcon width={14} height={19.7} />
+        <span>{profile?.timezone ?? 'Singapore GMT+8'}</span>
+      </div>
+      {open && <TimezoneModal setOpen={setOpen} />}
+    </>
   );
 };
 
