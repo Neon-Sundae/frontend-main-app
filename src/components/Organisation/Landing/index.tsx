08df617797f163992f35ebc12d9d7b6f2e3e1e82
anubhavadarsh <anubhav.adarsh9@gmail.com>
2022-06-24 10:42:38 +0530

    chore: start edit feature

--- src/components/Organisation/Landing/index.tsx
+++ src/components/Organisation/Landing/index.tsx
@@ -2,7 +2,9 @@ import BlurBlobs from 'components/BlurBlobs';
 import NavBar from 'components/NavBar';
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
+import Card from 'components/Card';
 import Banner from '../Banner';
+import CreatedBy from '../CreatedBy';
 import styles from './index.module.scss';
 
 const Landing: FC = () => {
@@ -13,7 +15,50 @@ const Landing: FC = () => {
       <BlurBlobs />
       <NavBar />
       <Banner />
+      <div className={styles.content}>
+        <section className={styles.section}>
+          <div className={styles.child}>
+            <p>Company Description</p>
+            <div>
+              <CompanyDesc />
+            </div>
+          </div>
+          <div className={styles.child}>
+            <p>Profile Created By</p>
+            <div>
+              <CreatedBy />
+            </div>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <div className={styles.child}>
+            <p className={styles.whitePaper}>White Paper</p>
+            <a href="http://#">
+              https://breath.example.com/brother/action.html?basketball=bridge&branch=arm
+            </a>
+          </div>
+        </section>
+      </div>
     </div>
+  );
+};
+
+const CompanyDesc = () => {
+  return (
+    <Card className={styles.description}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Magna eget est lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum
+        dolor sit amet.
+      </p>
+    </Card>
   );
 };
 
