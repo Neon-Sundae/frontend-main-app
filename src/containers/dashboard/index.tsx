ecf3ee6eab2cc19800eac93e07f48adbf32d5b18
Rahul <rahulcy7@gmail.com>
2022-06-28 16:09:26 +0530

    fix: remove profile & logout buttons

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -3,10 +3,9 @@ import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'reducers';
 import { Landing } from 'components/Home';
-import { useNavigate } from 'react-router-dom';
+
 import { removeAccessToken } from 'utils/authFn';
 const Dashboard: FC = () => {
-  const navigate = useNavigate();
   const isFirstTimeUser = useSelector(
     (state: RootState) => state.auth.isFirstTimeUser
   );
@@ -21,21 +20,6 @@ const Dashboard: FC = () => {
 
   return (
     <>
-      <button
-        onClick={() => {
-          navigate('/profile');
-        }}
-      >
-        Profile
-      </button>
-      <button
-        onClick={() => {
-          removeAccessToken();
-          navigate('/login');
-        }}
-      >
-        Logout
-      </button>
       <Landing />
     </>
   );
