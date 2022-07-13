19640a8265289e0487793b14d5f57e8685a40277
Rahul <rahulcy7@gmail.com>
2022-07-13 21:35:09 +0530

    feat: pass data to Tasks

--- src/components/Home/Landing/index.tsx
+++ src/components/Home/Landing/index.tsx
@@ -4,16 +4,27 @@ import Tasks from '../Tasks';
 import styles from './index.module.scss';
 import Banner from '../Banner';
 import Projects from '../Projects';
+import { useQuery } from 'react-query';
+import config from 'config';
+import { getAccessToken } from 'utils/authFn';
 
 const Landing: FC = () => {
-
+  const { isLoading, error, data, isFetching } = useQuery('newTasks', () =>
+    fetch(`${config.ApiBaseUrl}/task/new`, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${getAccessToken()}` },
+    }).then((response) => response.json())
+  );
+  if (isFetching) return <p>Loading...</p>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
   return (
     <div className={styles.background}>
       <NavBar />
       <Banner />
       <section className={styles.content}>
         <Projects />
-        <Tasks />
+        <Tasks data={data} />
       </section>
     </div>
   );
