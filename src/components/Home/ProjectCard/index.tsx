8c859a35e988a14d30dd720623914e456df395a8
Rahul <rahulcy7@gmail.com>
2022-07-15 07:05:44 +0530

    feat: separate project card for home

--- src/components/Home/ProjectCard/index.tsx
+++ src/components/Home/ProjectCard/index.tsx
@@ -6,7 +6,36 @@ import { useNavigate } from 'react-router-dom';
 
 const ProjectCard = (props: any) => {
   const navigate = useNavigate();
-  const { projectName, description, org, numTasks, projectId } = props;
+  const { projectName, description, org, numTasks, projectId, location } =
+    props;
+  if (location === 'home') {
+    return (
+      <Card
+        className={styles['project-card']}
+        showTransparentBg={true}
+        width={props.width}
+        marginRight={'50px'}
+      >
+        <>
+          <header>
+            <BrandImage width={70} height={70} />
+            <h3 className={styles['text--primary']}>{projectName}</h3>
+            <span className={styles['text--secondary']}>{org}</span>
+          </header>
+          <p className={styles['text-content']}>{description}</p>
+
+          <footer
+            onClick={() => {
+              navigate(`/project/${projectId}`);
+            }}
+          >
+            <span className={styles['text-extra']}>{numTasks} tasks</span>
+            <span className={clsx('material-icons', styles.icon)}>east</span>
+          </footer>
+        </>
+      </Card>
+    );
+  }
   return (
     <Card
       className={styles['project-card']}
