eb5adbe7f4659823633c949299ea40c5b30fedff
Rahul <rahulcy7@gmail.com>
2022-07-20 21:58:34 +0530

    feat: add moralis and moralis provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import reducer from 'reducers';
 import App from './App';
 import 'styles/main.scss';
+import { MoralisProvider } from "react-moralis";
 
 const store = createStore(reducer);
 const queryClient = new QueryClient();
@@ -13,7 +14,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <App />
+      <MoralisProvider serverUrl="https://gdosrk5keuuz.usemoralis.com:2053/server" appId="lJJWcluaU4bw2Pe0EA1z8maNh3NSxFUX0uYTFwz9">
+        <App />
+      </MoralisProvider>
     </Provider>
   </QueryClientProvider>
 );
