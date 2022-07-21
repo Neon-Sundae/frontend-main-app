38637ec3f5c15800f219cd3635432ca50eefa7c9
Rahul <rahulcy7@gmail.com>
2022-07-21 17:58:19 +0530

    feat: add profile pic update options

--- src/components/ProfileCard/BasicDetailsEdit/index.tsx
+++ src/components/ProfileCard/BasicDetailsEdit/index.tsx
@@ -4,9 +4,18 @@ import { ReactComponent as FoundersLabIcon } from 'assets/illustrations/icons/fo
 import { useSelector } from 'react-redux';
 import { RootState } from 'reducers';
 import styles from './index.module.scss';
-import useUpdateProfileDetails from './hooks';
+
 import toast from 'react-hot-toast';
 import useProfileManage from '../BasicDetails/hooks';
+import Modal from 'components/Modal';
+import PromptImage from 'assets/images/profile/prompt-img.png';
+
+import { Oval } from "react-loader-spinner";
+import { ReactComponent as EditIcon } from 'assets/illustrations/icons/edit.svg';
+import Background from 'assets/illustrations/profile/pp-bg.png';
+import getRandomString from "utils/getRandomString";
+
+import { useUpdateProfileDetails, fetchNFTs } from "./hooks";
 
 const BasicDetailsEdit: FC = () => {
   const { profile, profileContractAddress } = useSelector((state: RootState) => state.profile);
@@ -20,9 +29,10 @@ const BasicDetailsEdit: FC = () => {
   text is here imsum text is here imsum text is here imsum text is here
   imsum.`
   );
-
+  const [picture, setPicture] = useState(profile?.picture ?? userImage);
+  const [nfts, setNfts] = useState({});
+  const [agree, setAgree] = useState(false);
   const updateProfileDetails = useUpdateProfileDetails();
-
   const handleSave = () => {
     updateProfileDetails({
       userId: user?.userId,
@@ -30,12 +40,27 @@ const BasicDetailsEdit: FC = () => {
       name,
       title,
       description: bio,
+      picture: picture,
     });
   };
-
+  const walletId = useSelector((state: RootState) => state.user.user?.walletId);
+  const { data, isLoading, refetch } = fetchNFTs(walletId, agree);
+  const getNfts = () => {
+    if (Object.keys(nfts).length === 0) {
+      setAgree(true);
+      refetch();
+      setNfts(data);
+    }
+  };
   return (
     <>
-      <ProfileImage />
+      <ProfileImage
+        nfts={nfts}
+        picture={picture}
+        setPicture={setPicture}
+        getNFTs={getNfts}
+
+      />
       <NameDesignation
         title={title}
         setTitle={setTitle}
@@ -68,11 +93,121 @@ const SaveProfile: FC<ISaveProfile> = ({ handleSave }) => {
   );
 };
 
-const ProfileImage: FC = () => {
+interface IProfileImage {
+  nfts: any;
+  picture?: string;
+  setPicture: (picture: string) => void;
+  getNFTs: () => void;
+}
+
+const ProfileImage: FC<IProfileImage> = ({ nfts, picture, setPicture, getNFTs }) => {
+  const [profileModal, setProfileModal] = useState(false);
+  const [stepTwo, setStepTwo] = useState(false);
+  const [finStep, setFinStep] = useState(false);
+  const getTokenMetadata = (token: any) => {
+    const metadata = JSON.parse(token.metadata);
+    return {
+      name: metadata?.name,
+      description: metadata?.description,
+      externalLink: metadata?.external_link,
+      attributes: metadata?.attributes,
+      animation: metadata?.animation,
+      animationUrl: metadata?.animation_url,
+      image: metadata?.image,
+    };
+  }
+  const extractSelectedImageUri = (data: any) => {
+    if (!data.length) {
+      toast.error('NFT doesn\'t have image! Select another');
+    }
+    setPicture(data);
+  }
+  if (profileModal) {
+    return <Modal
+      onClose={() => setProfileModal(false)}
+      width="599px"
+      height="711px"
+    >
+      <div className={styles.wrapper}>
+        <img src={PromptImage} alt="Profile" className={styles.promptImg} />
+        <div className={styles.promptText}>
+          <h2>Show off your prized possessions</h2>
+          <p>Set your profile picture to an NFT you own by connnecting your wallet and verifiying address</p>
+          <div></div>
+          <footer>By connecting your wallet you authorise Founder’s Lab to extract your NFT information belonging to <a>0xb23bvu3
+            Learn More</a></footer>
+          <button onClick={(e) => { e.preventDefault(); setStepTwo(true); setProfileModal(false); getNFTs() }}>Allow Access</button>
+        </div>
+      </div>
+    </Modal >;
+  }
+  if (stepTwo) {
+    setTimeout(() => {
+      setFinStep(true);
+      setStepTwo(false);
+      setProfileModal(false);
+    }, 2000);
+    return <Modal
+      onClose={() => { setProfileModal(false); setStepTwo(false); }}
+      width="650px"
+      height="482.13px"
+      overflowY={'auto'}
+    >
+      <div className={styles.center}>
+        <h2>NFTs</h2>
+        <div className={styles.empty}></div>
+        <Oval
+          ariaLabel="loading-indicator"
+          height={96.7}
+          width={96.7}
+          strokeWidth={5}
+          strokeWidthSecondary={1}
+          color="#fff"
+          secondaryColor="none"
+        />
+        <p>Syncing your NFT Data...</p>
+      </div>
+    </Modal >;
+  }
+  if (finStep) {
+    return <Modal
+      onClose={() => { setProfileModal(false); setStepTwo(false); setFinStep(false) }}
+      width="599px"
+      height="711px"
+      overflowY={'auto'}
+    >
+      <div className={styles.nftsWrapper}>
+        <h2>NFTs</h2>
+        <div className={styles.nftsContainer} >
+          {nfts.result.map((token: any) => {
+            const metadata = getTokenMetadata(token);
+            return (
+              <><span className={styles.singleNft} key={getRandomString(5)} onClick={() => {
+                console.log('clicked!');
+                extractSelectedImageUri(metadata.image ? metadata.image.replace("ipfs://", "https://ipfs.io/ipfs/") : '');
+              }}  >
+                <img src={metadata.image ? metadata.image.replace("ipfs://", "https://ipfs.io/ipfs/") : userImage} alt="NFT" />
+                <h3>{metadata.name ? metadata.name : 'no data'}</h3>
+                <p>{metadata.description ? metadata.description : "no data"}</p>
+              </span></>
+            )
+          }
+          )}
+        </div>
+        <button onClick={() => {
+          setFinStep(false);
+          setStepTwo(false);
+          setProfileModal(false);
+        }}>Select NFT</button>
+      </div>
+    </Modal >;
+  }
   return (
-    <div className={styles['profile-image']}>
+    <div className={styles['profile-image']} onClick={() => setProfileModal(true)}>
       <div className={styles['image-wrapper']}>
-        <img alt="user" src={userImage} />
+        <img alt="user" src={picture ? picture : userImage} className={styles.userImage} />
+        <img alt="background" src={Background} className={styles.bgImage} />
+        <EditIcon className={styles.editIcon} />
       </div>
     </div>
   );
