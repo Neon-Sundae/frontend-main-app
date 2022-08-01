7a54a04c59e916887c9f99ed28a1539b1aff2497
Rahul <rahulcy7@gmail.com>
2022-08-01 09:14:25 +0530

    feat: component for project edit

---
+++ src/components/StartPrjModal/CreatePrjModalWithData/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import Modal from 'components/Modal';
+
+import EditProjectForm from '../EditProjectForm';
+
+interface CreatePrjModalWithDataProps {
+  onClose: () => void;
+}
+
+const CreatePrjModalWithData: FC<CreatePrjModalWithDataProps> = ({
+  onClose,
+}) => {
+  return (
+    <Modal onClose={onClose} width="700px" overflowY="auto" maxHeight="90vh">
+      <EditProjectForm />
+    </Modal>
+  );
+};
+
+export default CreatePrjModalWithData;
