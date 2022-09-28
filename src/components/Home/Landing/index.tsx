aebc1aae9f29d7d9156e6d3da1b92c3d171e5c02
Rahul <rahulcy7@gmail.com>
2022-09-28 21:00:35 +0530

    added check for new user in home page

--- src/components/Home/Landing/index.tsx
+++ src/components/Home/Landing/index.tsx
@@ -1,10 +1,14 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import NavBar from 'components/NavBar';
 import { useQuery } from '@tanstack/react-query';
 import config from 'config';
 import { getAccessToken } from 'utils/authFn';
 import BlurBlobs from 'components/BlurBlobs';
 import useFetchOffChainProfile from 'hooks/useFetchOffChainProfile';
+import Shepherd from 'shepherd.js';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from 'reducers';
 import Tasks from '../Tasks';
 import Banner from '../Banner';
 import Projects from '../Projects';
@@ -12,7 +16,49 @@ import styles from './index.module.scss';
 
 const Landing: FC = () => {
   // const { offChainProfile } = useFetchOffChainProfile();
-
+  const navigate = useNavigate();
+  const profile = useSelector((state: RootState) => state.profile.profile);
+  const [checkOnboardStatus, setCheckOnboardStatus] = useState(
+    localStorage.getItem('onboardStatus')
+  );
+  const tour = new Shepherd.Tour({
+    useModalOverlay: true,
+    defaultStepOptions: {
+      classes: 'shadow-md bg-purple-dark',
+      scrollTo: true,
+    },
+  });
+  const handleStepOne = () => {
+    tour.cancel();
+    navigate(`/profile/${profile?.profileId}`);
+    navigate(0);
+  };
+  tour.addStep({
+    id: 'step0',
+    arrow: true,
+    text: "Hello 👋,<br /> Let's get you first started by <br />minting your profile",
+    attachTo: {
+      element: '#navbar-wallet-information',
+      on: 'bottom',
+    },
+    classes: 'shepherd-theme-custom',
+    buttons: [
+      {
+        text: '1/5',
+        classes: 'information-button',
+      },
+      {
+        action() {
+          return handleStepOne();
+        },
+        text: 'Click to continue',
+      },
+      {
+        text: '⏩',
+        classes: 'information-button',
+      },
+    ],
+  });
   const { data } = useQuery(
     ['newTasks'],
     () =>
@@ -24,7 +70,13 @@ const Landing: FC = () => {
       refetchOnWindowFocus: false,
     }
   );
-
+  if (checkOnboardStatus && checkOnboardStatus === 'started') {
+    if (!tour.isActive()) {
+      setTimeout(() => {
+        tour.start();
+      }, 500);
+    }
+  }
   if (data) {
     return (
       <div className={styles.background}>
