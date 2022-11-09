e83eb5caf74ad7102b852b0f992ff93ac6ce1147
godmodefounder <priya@founderslab.xyz>
2022-11-09 13:03:29 +0800

    UX flows and Onramping
    1. New publish flow
    2. UX deposit and withdrawal flow Metamask only
    3. Onramping modal corrected with correct svgs.
    
    To do: Events on Withdrawal and Deposit flows so that the project can update.

--- src/utils/contractFns/depositProjectFunds.ts
+++ src/utils/contractFns/depositProjectFunds.ts
@@ -3,11 +3,13 @@ import { getWeb3Instance } from 'utils/web3EventFn';
 import ProjectAbi from 'contracts/abi/Project.sol/Project.json';
 import USDCAbi from 'contracts/abi/USDC.sol/USDC.json';
 import { USDCAddress } from 'contracts/contracts';
+import { SetStateAction, Dispatch } from 'react';
 
 const depositProjectFunds = async (
   amount: number,
   contractAddress: string,
-  userAddress: string
+  userAddress: string,
+  setDeploying: Dispatch<SetStateAction<string>>
 ) => {
   const web3 = getWeb3Instance();
   const depositAmount = amount * 10 ** 6;
@@ -24,11 +26,25 @@ const depositProjectFunds = async (
 
   await USDCContract.methods
     .approve(contractAddress, depositAmount)
-    .send({ from: userAddress });
+    .send({ from: userAddress })
+    .on('transactionHash', () => {
+      setDeploying('give_usdc_permission');
+    })
+    .on('receipt', async (receipt: any) => {
+      setDeploying('usdc_success');
+      console.log(JSON.stringify(receipt.events));
+    });
 
   await ProjectContract.methods
     .depositFunds(depositAmount)
-    .send({ from: userAddress });
+    .send({ from: userAddress })
+    .on('transactionHash', () => {
+      setDeploying('deploying');
+    })
+    .on('receipt', async (receipt: any) => {
+      setDeploying('deploy_success');
+      console.log(JSON.stringify(receipt.events));
+    });
 
   return 'Success';
 };
