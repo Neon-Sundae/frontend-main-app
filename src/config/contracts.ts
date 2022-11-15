0cff46cc52592d6facebef16d808bce8302492d3
GermaVinsmoke <vaibhav1180@gmail.com>
2022-11-15 15:02:17 +0530

    added testnet contracts
    Signed-off-by: GermaVinsmoke <vaibhav1180@gmail.com>

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -23,15 +23,15 @@ export const devContracts = {
 };
 
 export const testnetContracts = {
-  profileImplementationAddress: '0xf47f43D0955E608D6020Dc2F4f7110cE402cBc37',
-  profileFactoryAddress: '0x57fE39DfBCd7556f4F85f92Cc1461F4891c8d513',
-  projectImplementationAddress: '0x60D1Ca649AaD981aB4b2fbA3db57f0211d845bd3',
-  projectFactoryAddress: '0xF6288562D1061C72497D5058ed6C2773769646dE',
-  taskImplementationAddress: '0x2843E2D32347C5B75d7895c45Aa5B294b24B61C3',
-  taskFactoryAddress: '0xeAd0a5e6303aB1c2f97E2234c42da3E7C296bD1B',
-  USDCAddress: '0x9E5C360f315A1E23322bB330C9af87dde77ef7D9',
-  FNDRAddress: '0x7d130Ce789803b5007994009E60984f8E10FE2Dc',
-  FLAddress: '0x4aae4342Bf18B07962f54D4a4437d5F0DAb8D0Ac',
+  profileImplementationAddress: '0xd612a87f446Cd0f4F435B0398886c1a629920dD5',
+  profileFactoryAddress: '0xce0712a06a747C0c4b9b10B678ADbd63F4E83BB4',
+  projectImplementationAddress: '0x12c27122c74A52Bcd77b8B8b65d5b7f1D814670C',
+  projectFactoryAddress: '0xf82C31e21380a887163F355371476d37E8fEb4ec',
+  taskImplementationAddress: '0x6Fa2e1bf42fd5a904C74B6e5a9E70eaa671257F8',
+  taskFactoryAddress: '0x517E4FcfF2903EC2886323Bf1Aa46448C6dF93C1',
+  USDCAddress: '0x9Fde4d35C656e69cbb2bbCE6f87BE1021D26a7e3',
+  FNDRAddress: '0xBE77B71FD0132605b711deAaDA1bFAF82bF44fb6',
+  FLAddress: '0xC0Ba3f0d6fce1cBf05c02A30afaB883ad006BEC1',
 };
 
 export const mainnetContracts = {
