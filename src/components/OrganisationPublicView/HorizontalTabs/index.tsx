12b4d4ed1498b7a262f55509540913dfa32834c2
Rahul <rahulcy7@gmail.com>
2023-03-16 15:26:04 +0530

    styles: horizontal tabs

--- src/components/OrganisationPublicView/HorizontalTabs/index.tsx
+++ src/components/OrganisationPublicView/HorizontalTabs/index.tsx
@@ -1,4 +1,6 @@
 import { selectedOrganisationPageTab } from 'actions/organisation';
+import clsx from 'clsx';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styles from './index.module.scss';
 
@@ -16,18 +18,30 @@ interface TabItem {
 
 const HorizontalTabs = () => {
   const dispatch = useDispatch();
+  const [buttonSelected, setButtonSelected] = useState(0);
 
   const handleButtonClick = (tabItem: TabItem) => {
+    setButtonSelected(tabItem.id);
     dispatch(selectedOrganisationPageTab(tabItem.id));
   };
 
   return (
     <div className={styles['horizontal-tabs']}>
       {tabs.map(tabItem => (
-        <button key={tabItem.id} onClick={() => handleButtonClick(tabItem)}>
+        <button
+          key={tabItem.id}
+          onClick={() => handleButtonClick(tabItem)}
+          className={clsx(
+            buttonSelected === tabItem.id
+              ? styles['button-active']
+              : styles['button-inactive']
+          )}
+        >
           {tabItem.text}
         </button>
       ))}
+
+      <button className={styles['custom-button']}>Custom Button</button>
     </div>
   );
 };
