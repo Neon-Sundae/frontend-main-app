b191ee0a4e01ddd4902a3952806951637a869f0b
Rahul <rahulcy7@gmail.com>
2023-05-02 16:53:22 +0530

    fix: lottie animation resize issue

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,21 +1,15 @@
 import { FC } from 'react';
 import Lottie from 'lottie-react';
 import loading from 'assets/lottie/loading.json';
-import messages from 'assets/data/loading.json';
-import emojis from 'utils/emojis';
 import BlurBlobs from 'components/BlurBlobs';
 import styles from './index.module.scss';
 
 const Loading: FC = () => {
-  const x = Math.floor(Math.random() * messages.length + 1);
   return (
     <div className={styles.backdrop}>
       <BlurBlobs />
       <div className={styles.overlay}>
         <Lottie animationData={loading} />
-        <p>
-          {messages[x]} {emojis[x]}
-        </p>
       </div>
     </div>
   );
