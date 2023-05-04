15edc4151a21f75026903767990ece37511ecd18
Rahul <rahulcy7@gmail.com>
2023-05-04 11:12:03 +0530

    add new form component

---
+++ src/components/StartPrjModal/EditProjectForm/FormComponent/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+const FormComponent = () => {
+  return (
+    <div className={styles['edit-project-form-container']}>Form Component</div>
+  );
+};
+
+export default FormComponent;
