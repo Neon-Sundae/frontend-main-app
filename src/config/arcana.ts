10a7b04d1640269bd146ee05e03f39f11c028cee
Rahul <rahulcy7@gmail.com>
2023-07-31 14:13:42 +0530

    debug hardcode rpc url

--- src/config/arcana.ts
+++ src/config/arcana.ts
@@ -1,12 +1,12 @@
 import { AuthProvider } from '@arcana/auth';
-import config from 'config';
+// import config from 'config';
 
 export const arcanaProvider = new AuthProvider(
   `${import.meta.env.VITE_ARCANA_AUTH_CLIENT_KEY}`,
   {
     chainConfig: {
       chainId: '0x13881',
-      rpcUrl: config.rpcURL,
+      rpcUrl: 'https://rpc-mumbai.maticvigil.com',
     },
     network: import.meta.env.VITE_ARCANA_NETWORK,
   }
